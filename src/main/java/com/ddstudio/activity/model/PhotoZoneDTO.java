c303de1f62515ae7dd58ab639949bf072f4144da
NaraeP <skfocjstk93@naver.com>
2023-11-08 15:50:32 +0900

    Update. 08NOV23 activity 작업중

---
+++ src/main/java/com/ddstudio/activity/model/PhotoZoneDTO.java
@@ -0,0 +1,13 @@
+package com.ddstudio.activity.model;
+
+import lombok.Data;
+
+@Data
+public class PhotoZoneDTO {
+
+	private String photozoen_seq;
+	private String name;
+	private String time;
+	private String info;
+	private String location_seq;
+}
