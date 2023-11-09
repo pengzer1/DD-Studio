824512cc45ad010729b4d09ee5e84cbabdc3b451
dmsalsgus <leeje971118@naver.com>
2023-11-09 11:15:57 +0900

    add : DAO 추가

--- src/main/java/com/ddstudio/guide/repository/BusDAO.java
+++ src/main/java/com/ddstudio/guide/repository/BusDAO.java
@@ -1,5 +1,19 @@
 package com.ddstudio.guide.repository;
 
-public class BusDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+import com.ddstudio.DBUtil;
+
+public class BusDAO {
+	private Connection conn;
+	private Statement stat;
+	private PreparedStatement pstat;
+	private ResultSet rs;
+	
+	public BusDAO() {
+		this.conn=DBUtil.open();
+	}
 }
