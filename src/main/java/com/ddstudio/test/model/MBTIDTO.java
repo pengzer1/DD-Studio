591dbedd19923b0971299581d176ca6f943f4415
Isaac-Seungwon <zhzkdkrak@naver.com>
2023-11-12 21:37:29 +0900

    feat: test 관련 DTO 파일 생성

---
+++ src/main/java/com/ddstudio/test/model/MBTIDTO.java
@@ -0,0 +1,50 @@
+package com.ddstudio.test.model;
+
+public class MBTIDTO {
+	
+	public String mbti_seq;
+	public String result;
+	public String mbti;
+	public String course_seq;
+	public String attraction_seq;
+	
+	public String getMbti_seq() {
+		return mbti_seq;
+	}
+	
+	public void setMbti_seq(String mbti_seq) {
+		this.mbti_seq = mbti_seq;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
+	}
+	
+	public String getMbti() {
+		return mbti;
+	}
+	
+	public void setMbti(String mbti) {
+		this.mbti = mbti;
+	}
+	
+	public String getCourse_seq() {
+		return course_seq;
+	}
+	
+	public void setCourse_seq(String course_seq) {
+		this.course_seq = course_seq;
+	}
+	
+	public String getAttraction_seq() {
+		return attraction_seq;
+	}
+	
+	public void setAttraction_seq(String attraction_seq) {
+		this.attraction_seq = attraction_seq;
+	}
+}
