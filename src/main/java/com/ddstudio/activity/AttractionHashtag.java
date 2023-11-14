6a0ac13320e2e38a0258ccac1e779f2445046dac
NaraeP <skfocjstk93@naver.com>
2023-11-14 17:19:58 +0900

    Add. 14MOV23 어트랙션 이미지 추가

---
+++ src/main/java/com/ddstudio/activity/AttractionHashtag.java
@@ -0,0 +1,24 @@
+package com.ddstudio.activity;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/activity/attractionhashtag.do")
+public class AttractionHashtag extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//AttractionHashtag.java
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/activity/attractionhashtag.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+}
